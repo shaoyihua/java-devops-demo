6a1eaa5336820dd1a4f65afa5edc90368d188bae
“AndyL-OfficeAllinone” <AndyL@al.com>
2023-12-06 12:30:20 +0800

    java devops init commit

---
+++ src/main/java/com/itheigougou/javadevopsdemo/JavaDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.itheigougou.javadevopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopsDemoApplication.class, args);
+    }
+
+}
